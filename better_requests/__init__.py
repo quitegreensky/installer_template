de768c29ab62a51ce22f5e56682f340ac3adf662
quitegreensky <quitegreensky@gmail.com>
2025-03-28 16:42:48 +0330

    revert

--- better_requests/__init__.py
+++
@@ -1,8 +0,0 @@
-__version__ = '1.0.0'
-__description__ = 'python requests with retry and proxy support'
-__author__= "Unknown"
-__email__="Unknown@gmail.com"
-__url__ = "https://github.com/quitegreensky/better_requests"
-__package_name__ = 'better_requests'
-
-from better_requests.better_requests import BetterRequests
